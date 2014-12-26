329652692f5478c37990c6f210ccbfca9db9cdd3
Andrew Seeley <awseeley@gmail.com>
2014-12-27 10:48:16 +1100

    Swift Simple Calculator Tutorial
    Swift Simple Calculator Tutorial

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var lblResult: UITextField!
+    
+    var result = Float()
+    var currentNumber = Float()
+    
+    var currentOp = String()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentOp = "="
+        lblResult.text = ("\(result)")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNumberInput(sender: UIButton) {
+        currentNumber = currentNumber * 10 + Float(sender.titleLabel!.text!.toInt()!)
+        lblResult.text = ("\(currentNumber)")
+    }
+    
+    @IBAction func btnOperation(sender: UIButton) {
+        
+        switch currentOp {
+            case "=":
+                result = currentNumber
+            case "+":
+                result = result + currentNumber
+            case "-":
+                result = result - currentNumber
+            case "*":
+                result = result * currentNumber
+            case "/":
+                result = result / currentNumber
+            default:
+                println("error")
+        }
+        
+        currentNumber = 0
+        lblResult.text = ("\(result)")
+        
+        if(sender.titleLabel!.text == "=") {
+            result = 0
+        }
+        
+        currentOp = sender.titleLabel!.text! as String!
+        
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
+        result = 0
+        currentNumber = 0
+        currentOp = "="
+        lblResult.text = ("\(result)")
+    }
+    
 
 }
 
